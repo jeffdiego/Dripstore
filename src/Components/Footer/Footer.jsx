7ce6d161f0eec231c93d9ff12bef500b968c67f7
MARIA CLARA ABREU LEITÃO <marialeitao0992@digitalcollege.internal>
2026-06-01 20:28:15 -0300

    div 1

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,19 +1,26 @@
 import Logo from "../../assets/logo.png"
 
 function Footer() {
-  
   return (
-    <>
-      <section>
-        <div>
-            <img src="logo" alt="Logo" />
-            <p>Tecnolgia que conecta você ao <br>
-            </br>que hpa de melhor no mundo</p>
-        </div>
-       
-      </section>
-    </>
-  )
+    <section className="footer-social">
+      <div>
+        <img src="/logo.png" alt="Logo" />
+
+        <p>
+          Tecnologia que conecta você ao
+          <br />
+          que há de melhor no mundo.
+        </p>
+      </div>
+
+      <div className="social-icons">
+        <i className="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-facebook-f"></i>
+        <i className="fa-brands fa-youtube"></i>
+        <i className="fa-brands fa-tiktok"></i>
+      </div>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
