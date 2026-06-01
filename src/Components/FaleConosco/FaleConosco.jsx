85827b7147eeeb6f69e1f3e1e348afe061905bec
Visitante Aldeota <visitante@digitalcollege.internal>
2026-06-01 19:29:12 -0300

    Fale conosco

---
+++ src/Components/FaleConosco/FaleConosco.jsx
@@ -0,0 +1,25 @@
+import { FaEnvelope } from "react-icons/fa";
+function FaleConosco() {
+
+    return (
+        <>
+            <section>
+                <div >
+                    <h1>Fale Conosco</h1>
+
+                    <p>Cadastre-se e receba as melhores ofertas e lançamentos em primeira mão.</p>
+
+                    <FaEnvelope />
+
+                </div>
+                <div>
+            
+                </div>
+
+            </section>
+        </>
+
+    )
+}
+
+export default FaleConosco
