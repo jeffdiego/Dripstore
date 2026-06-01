4000fe7057cf91d0aa9cd170c608619c3333822d
ronaldo charles de sousa leitão <ronaldoleito0079@digitalcollege.internal>
2026-06-01 19:58:14 -0300

    footer div1

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import Logo from "../../assets/logo.png"
+
+function Footer() {
+  
+  return (
+    <>
+      <section>
+        <div>
+            <img src="logo" alt="Logo" />
+            <p>Tecnolgia que conecta você ao <br>
+            </br>que hpa de melhor no mundo</p>
+        </div>
+       
+      </section>
+    </>
+  )
+}
+
+export default Footer
