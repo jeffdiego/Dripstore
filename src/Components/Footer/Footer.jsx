d650fc29b2c405663c76b33da91544a5b915298c
ronaldo charles de sousa leitão <ronaldoleito0079@digitalcollege.internal>
2026-06-01 21:54:07 -0300

    feat/footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,75 +1,122 @@
-import Logo from "../../assets/logo.png"
-import Amex from "../../assets/amex.png"
-import Boleto from "../../assets/boleto.png"
-import Elo from "../../assets/elo.png"
-import Master from "../../assets/master.png"
-import Pix from "../../assets/pix.png"
-import Visa from "../../assets/visa.png"
+import Logo from "../../assets/logo.png";
+import Amex from "../../assets/amex.png";
+import Boleto from "../../assets/boleto.png";
+import Elo from "../../assets/elo.png";
+import Master from "../../assets/master.png";
+import Pix from "../../assets/pix.png";
+import Visa from "../../assets/visa.png";
+import Blindado from "../../assets/blindado.png";
+import Google from "../../assets/google.png";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faInstagram,
-  faFacebookF,
-  faYoutube,
-  faTiktok,
+    faInstagram,
+    faFacebookF,
+    faYoutube,
+    faTiktok,
 } from "@fortawesome/free-brands-svg-icons";
+
+import "./Footer.css";
+
 function Footer() {
     return (
-        <>
-            <section className="footer-social">
-                <div>
-                    <img src={Logo} alt="" />
+        <footer className="footer">
+            <div className="footer-container">
+
+                <div className="footer-brand">
+                    <img src={Logo} alt="Drip Store" className="footer-logo" />
 
                     <p>
                         Tecnologia que conecta você ao
                         <br />
                         que há de melhor no mundo.
                     </p>
+
+                    <div className="social-icons">
+                        <a href="#">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+
+                        <a href="#">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+
+                        <a href="#">
+                            <FontAwesomeIcon icon={faYoutube} />
+                        </a>
+
+                        <a href="#">
+                            <FontAwesomeIcon icon={faTiktok} />
+                        </a>
+                    </div>
                 </div>
 
-                <div className="social-icons">
-                    <FontAwesomeIcon icon={faInstagram} />
-                    <FontAwesomeIcon icon={faFacebookF} />
-                    <FontAwesomeIcon icon={faYoutube} />
-                    <FontAwesomeIcon icon={faTiktok} />
+
+                <div className="footer-column">
+                    <h3>Institucional</h3>
+
+                    <ul>
+                        <li>Sobre Nós</li>
+                        <li>Trabalhe Conosco</li>
+                        <li>Política de Privacidade</li>
+                        <li>Termos de Uso</li>
+                    </ul>
                 </div>
 
-                <div>
-                    <h2>Institucional</h2>
-                    <p>Sobre Nós</p>
-                    <p>Trabalhe Conosco</p>
-                    <p>Politica de Privacidade</p>
-                    <p>Termos de Uso</p>
+
+                <div className="footer-column">
+                    <h3>Ajuda</h3>
+
+                    <ul>
+                        <li>Central de Ajuda</li>
+                        <li>Formas de Pagamento</li>
+                        <li>Prazo de Entrega</li>
+                        <li>Trocas e Devoluções</li>
+                    </ul>
                 </div>
 
-                <div>
-                    <h2>Ajuda</h2>
-                    <p>Central de ajuda</p>
-                    <p>Fromas de Pagamentos</p>
-                    <p>Prazo de entregas</p>
-                    <p>Trocas e devoluções</p>
+
+                <div className="footer-column">
+                    <h3>Minha Conta</h3>
+
+                    <ul>
+                        <li>Meus Pedidos</li>
+                        <li>Meus Dados</li>
+                        <li>Lista de Desejos</li>
+                        <li>Endereços</li>
+                    </ul>
                 </div>
 
-                <div>
-                    <h2>Minha Conta</h2>
-                    <p>Meus pedidos</p>
-                    <p>Meus dadoss</p>
-                    <p>Lista de desejo</p>
-                    <p>Ebdereços</p>
+
+                <div className="footer-column">
+                    <h3>Formas de Pagamento</h3>
+
+                    <div className="payment-methods">
+                        <img src={Amex} alt="American Express" />
+                        <img src={Boleto} alt="Boleto" />
+                        <img src={Elo} alt="Elo" />
+                        <img src={Master} alt="Mastercard" />
+                        <img src={Pix} alt="Pix" />
+                        <img src={Visa} alt="Visa" />
+                    </div>
                 </div>
 
-                <div>
-                    <h2>Forma de pagamento</h2>
-                    <img src={Amex} alt="amex" />
-                    <img src={Boleto} alt="Boleto" />
-                    <img src={Elo} alt="Elo" />
-                    <img src={Master} alt="Master" />
-                    <img src={Pix} alt="pix" />
-                    <img src={Visa} alt="Visa" />
-                </div>
+                <div className="footer-column">
+                    <h3>Segurança</h3>
 
-            </section>
-        </>
-    )
+                    <div className="security-methods">
+                        <img src={Blindado} alt="Seguro Blindado" />
+                        <img src={Google} alt="Google Site Seguro" />
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="footer-bottom">
+                <p>© 2025 Drip Store. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
