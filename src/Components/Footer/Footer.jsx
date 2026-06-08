d51067db10bdce9d31f8688c3a9ff55f637f44a8
MARIA CLARA ABREU LEITÃO <marialeitao0992@digitalcollege.internal>
2026-06-08 19:11:21 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,113 +10,109 @@ import Google from "../../assets/google.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faInstagram,
-    faFacebookF,
-    faYoutube,
-    faTiktok,
+  faInstagram,
+  faFacebookF,
+  faYoutube,
+  faTiktok,
 } from "@fortawesome/free-brands-svg-icons";
 
 import "./Footer.css";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="footer-container">
+  return (
+    <footer id="footer">
+      <div className="footer-container">
 
-                <div className="footer-brand">
-                    <img src={Logo} alt="Drip Store" className="footer-logo" />
+        <div className="footer-brand">
+          <img src={Logo} alt="TechStore" className="footer-logo" />
 
-                    <p>
-                        Tecnologia que conecta você ao
-                        <br />
-                        que há de melhor no mundo.
-                    </p>
+          <p>
+            Tecnologia que conecta você ao
+            <br />
+            que há de melhor no mundo.
+          </p>
 
-                    <div className="social-icons">
-                        <a href="#">
-                            <FontAwesomeIcon icon={faInstagram} />
-                        </a>
+          <div className="social-icons">
+            <a href="#">
+              <FontAwesomeIcon icon={faInstagram} className="icon-size" />
+            </a>
 
-                        <a href="#">
-                            <FontAwesomeIcon icon={faFacebookF} />
-                        </a>
+            <a href="#">
+              <FontAwesomeIcon icon={faFacebookF} className="icon-size" />
+            </a>
 
-                        <a href="#">
-                            <FontAwesomeIcon icon={faYoutube} />
-                        </a>
+            <a href="#">
+              <FontAwesomeIcon icon={faYoutube} className="icon-size" />
+            </a>
 
-                        <a href="#">
-                            <FontAwesomeIcon icon={faTiktok} />
-                        </a>
-                    </div>
-                </div>
+            <a href="#">
+              <FontAwesomeIcon icon={faTiktok} className="icon-size" />
+            </a>
+          </div>
+        </div>
 
+        <div className="footer-column">
+          <h3>Institucional</h3>
 
-                <div className="footer-column">
-                    <h3>Institucional</h3>
+          <ul>
+            <li>Sobre nós</li>
+            <li>Trabalhe conosco</li>
+            <li>Política de privacidade</li>
+            <li>Termos de uso</li>
+          </ul>
+        </div>
 
-                    <ul>
-                        <li>Sobre Nós</li>
-                        <li>Trabalhe Conosco</li>
-                        <li>Política de Privacidade</li>
-                        <li>Termos de Uso</li>
-                    </ul>
-                </div>
+        <div className="footer-column">
+          <h3>Ajuda</h3>
 
+          <ul>
+            <li>Central de ajuda</li>
+            <li>Formas de pagamento</li>
+            <li>Prazo de entrega</li>
+            <li>Trocas e devoluções</li>
+          </ul>
+        </div>
 
-                <div className="footer-column">
-                    <h3>Ajuda</h3>
+        <div className="footer-column">
+          <h3>Minha conta</h3>
 
-                    <ul>
-                        <li>Central de Ajuda</li>
-                        <li>Formas de Pagamento</li>
-                        <li>Prazo de Entrega</li>
-                        <li>Trocas e Devoluções</li>
-                    </ul>
-                </div>
+          <ul>
+            <li>Meus pedidos</li>
+            <li>Meus dados</li>
+            <li>Lista de desejos</li>
+            <li>Endereços</li>
+          </ul>
+        </div>
 
+        <div className="footer-column">
+          <h3>Formas de pagamento</h3>
 
-                <div className="footer-column">
-                    <h3>Minha Conta</h3>
+          <div className="payment-methods">
+            <img src={Visa} alt="Visa" />
+            <img src={Master} alt="Mastercard" />
+            <img src={Elo} alt="Elo" />
+            <img src={Amex} alt="American Express" />
+            <img src={Pix} alt="Pix" />
+            <img src={Boleto} alt="Boleto" />
+          </div>
+        </div>
 
-                    <ul>
-                        <li>Meus Pedidos</li>
-                        <li>Meus Dados</li>
-                        <li>Lista de Desejos</li>
-                        <li>Endereços</li>
-                    </ul>
-                </div>
+        <div className="footer-column">
+          <h3>Segurança</h3>
 
+          <div className="security-methods">
+            <img src={Blindado} alt="Site Blindado" />
+            <img src={Google} alt="Google Site Seguro" />
+          </div>
+        </div>
 
-                <div className="footer-column">
-                    <h3>Formas de Pagamento</h3>
+      </div>
 
-                    <div className="payment-methods">
-                        <img src={Amex} alt="American Express" />
-                        <img src={Boleto} alt="Boleto" />
-                        <img src={Elo} alt="Elo" />
-                        <img src={Master} alt="Mastercard" />
-                        <img src={Pix} alt="Pix" />
-                        <img src={Visa} alt="Visa" />
-                    </div>
-                </div>
-
-                <div className="footer-column">
-                    <h3>Segurança</h3>
-
-                    <div className="security-methods">
-                        <img src={Blindado} alt="Seguro Blindado" />
-                        <img src={Google} alt="Google Site Seguro" />
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="footer-bottom">
-                <p>© 2025 Drip Store. Todos os direitos reservados.</p>
-            </div>
-        </footer>
-    );
+      <p className="footer-copy">
+        © 2024 TechStore. Todos os direitos reservados.
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
