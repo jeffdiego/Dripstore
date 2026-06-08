18e524a8bd0e69b73e6d9c725708b54762ea6be4
promotoraqueiroz@gmail.com <promotoraqueiroz@gmail.com>
2026-06-08 20:35:36 -0300

    ajusteproductdetails

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,9 +1,13 @@
+import Footer from "../Components/Footer/Footer"
+import Header from "../Components/Header/Header"
 import ProductDescription from "../Components/ProductDescription/ProductDescription"
 function ProductDetails() {
 
   return (
     <>
+      <Header></Header>
       <ProductDescription></ProductDescription>
+      <Footer></Footer>
     </>
   )
 }
